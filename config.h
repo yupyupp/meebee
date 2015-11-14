e5d397b4664233c7409c5c6966e323dd638a5b98
Stephen Haffner <dev@kd8zev.net>
2015-11-14 15:23:46 -0500

    Config update

--- config.h
+++ config.h
@@ -1,3 +1,7 @@
+#define USERNAME_MAX 32
+
 #define SALTED_PASS_SIZE 21
 #define SALT_SIZE 12
-#define DATA_ROOT /opt/meebee
+
+#define DATA_ROOT /opt/meebee/
+#define DATABASE_FILE "DATA_ROOT/users.db"
--- data/data.c
+++ data/data.c
@@ -1,5 +1,32 @@
-int get_pass(const char* username, char** saltpass, char** salt) {
+#include <stdlib.h>
+#include <stdio.h>
+#include <crypt.h>
+#include <string.h>
+#include <sqlite3.h>
 
+#include "../config.h"
+
+int get_pass(const char* username, char** saltpass, char** salt) {
+    sqlite3 *db;
+    char* db_command;
+    char* error_msg;
+
+    /* TODO - possible multithreading issue location */
+    if (sqlite3_open(DATABASE_FILE, &db)) {
+        perror("Failed to open database");
+        sqlite_close(db);
+    }
+
+    if (sqlite3_exec(db, db_command, )) {
+        perror("Failed to open database");
+        sqlite_close(db);
+    }
+
+
+
+
+
+    sqlite3_close(db);
 
     return 0;
 }

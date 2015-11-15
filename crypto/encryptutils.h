f1788274119809c85edb8fec04d77315ffbed430
Daniel Gratz <gratz.dhg@gmail.com>
2015-11-15 01:51:58 -0500

    adding crypto core

---
+++ crypto/encryptutils.h
@@ -0,0 +1,27 @@
+#ifndef encriptutils
+#define encriptutils
+#include <gcrypt.h>
+#include <openssl/rsa.h>
+
+/*for a new user generate the shash*/
+char* new_shash(char *password, char *salt);
+
+/*generate a random salt*/
+void salt_generator(char** salt);
+
+/*given priv_key and salted hash writes encrypted_priv_key to aes encrypted priv_key*/
+int encrypt_private_key(char** priv_key, char** encrypted_priv_key, char* aesSymKey, size_t io_str_len);
+
+/*decrypts encrypted_priv_key with aesSymKey and stors it in priv_key*/
+int decrypt_private_key(char** priv_key, char** encrypted_priv_key, char* aesSymKey, size_t io_str_len);
+
+/*encrypts a file and stores it to DATA_ROOT/username/path-to-file*/
+int encrypt_file(RSA* key, char* filename, char* data, int datalen);
+
+/*decrypts a file and returns a pointer to the file in memeory*/
+int decrypt_file(RSA* key, char* filename, char* data, int datalen);
+
+/*generate a new public private keypair*/
+int gen_keypair(char* priv_key, char* pub_key, size_t key_len);
+
+#endif
---
+++ crypto/testmain.c
@@ -0,0 +1,49 @@
+#include "encriptutils.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <crypt.h>
+#include <limits.h>
+#include <gcrypt.h>
+#include <gpgme.h>
+#include <openssl/rsa.h>                                                                            
+#include <openssl/pem.h>
+
+/*for testing only*/
+int main(void) {
+
+  char* password;
+    int i = 0;
+
+    char* salt = (char*)malloc(sizeof(char) * 12);
+    strncpy(salt, "$1$........", 12);
+    salt_generator(&salt);
+    /* Read in the user’s password*/
+    password = getpass("Password:");
+    password = new_shash(password, salt);
+    password = password +12;
+    printf("hashed pass: %s\n", password);
+    printf("salt is: %s\n", salt);
+    char* test_priv_key = (char *)malloc(sizeof(char) *128);
+    strncpy(test_priv_key,"123456789 abcdefghijklmnopqrstuvwzyz ABCDEFGHIJKLMNOPQRSTUVWZYZ 123456789 abcdefghijklmnopqrstuvwzyz ABCDEFGHIJKLMNOPQRSTUVWZYZ", 128);
+    char* encrypt_priv_key = (char*)malloc(strlen(test_priv_key) +1);
+    puts(test_priv_key);
+    encrypt_private_key(&test_priv_key, &encrypt_priv_key, password, 128);
+    for (i = 0; i<128; i++) {
+        test_priv_key[i] = '0';                                                       
+        printf("%02X", (unsigned char)encrypt_priv_key[i]);
+    }
+    printf("\n");
+    decrypt_private_key(&test_priv_key, &encrypt_priv_key, password, 128);
+    puts("---------------\n");
+    puts(test_priv_key);
+
+    RSA* key = RSA_new();
+    BIGNUM* e = BN_new();
+    
+    RSA_generate_key_ex(key, 1024, e , NULL);
+    encrypt_file(key, "/home/dgratz/test1232.txt", test_priv_key, strlen(test_priv_key));
+    RSA_free(key);
+}    
